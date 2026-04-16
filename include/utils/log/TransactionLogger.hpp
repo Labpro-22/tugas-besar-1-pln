f22349b49b527ddbded9593cb8b9fcb8a9dd3c8c
Muhammad Nafis Habibi <137036100+NafisKreatif@users.noreply.github.com>
2026-04-16 15:47:48 +0700

    feat: add transaction logger

---
+++ include/utils/log/TransactionLogger.hpp
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <vector>
+
+#include "utils/log/TransactionLog.hpp"
+
+class TransactionLogger
+{
+private:
+    std::vector<TransactionLog> logs;
+public:
+    void log(int turn, std::string username, std::string action, std::string details);
+    const std::vector<TransactionLog>& getLogs() const;
+};
