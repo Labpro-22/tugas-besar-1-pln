6253119f9e6c0d56c557ff97b0dce4d635e64cd9
Muhammad Nafis Habibi <137036100+NafisKreatif@users.noreply.github.com>
2026-04-17 22:36:50 +0700

    feat: give initial money in Bank

--- src/models/Bank.cpp
+++ src/models/Bank.cpp
@@ -2,15 +2,21 @@
 
 Bank::Bank(long long initialMoney) : initialMoney{initialMoney} {}
 
+
+void Bank::giveInitialMoney(std::vector<Player> &players)
+{
+    giveMoneyToAll(players, initialMoney);
+}
+
 void Bank::giveMoney(Player &player, long long amount)
 {
-    player.getMoney(amount);
+    player.receiveMoney(amount);
 }
 
 void Bank::giveMoneyToAll(std::vector<Player> &players, long long amount)
 {
     for (Player &player : players) {
-        player.getMoney(amount);
+        player.receiveMoney(amount);
     }
 }
 
