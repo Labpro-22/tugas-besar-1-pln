2bd4ff6bbd264f8067cdce56fb8cb6de0b5591aa
wzlyy <wildanag.10@gmail.com>
2026-04-17 19:22:30 +0700

    fix: const method

--- include/views/DiceView.hpp
+++ include/views/DiceView.hpp
@@ -7,6 +7,7 @@ private:
     GameManager& gameManager;
 public:
     DiceView(GameManager& gm) : gameManager(gm) {}
-    void outputRolledDice(int value1, int value2);
-    void outputSetDice(int value1, int value2);
+    void outputRollDice() const;
+    void outputRolledDice(int value1, int value2) const;
+    void outputSetDice(int value1, int value2) const;
 };
--- include/views/LoadView.hpp
+++ include/views/LoadView.hpp
@@ -7,7 +7,7 @@ private:
     GameManager& gameManager;
 public:
     LoadView(GameManager& gm) : gameManager(gm) {}
-    void outputLoading();
-    void outputLoadStatus(bool success);
+    void outputLoading() const;
+    void outputLoadStatus(bool success) const;
 };
 
