91d538a92a3f9fbc32945c7cf43c50acced09af7
Muhammad Nafis Habibi <137036100+NafisKreatif@users.noreply.github.com>
2026-04-18 21:47:27 +0700

    feat: SaveFileHandler hpp

--- include/utils/save-load/SaveFileHandler.hpp
+++ include/utils/save-load/SaveFileHandler.hpp
@@ -1,12 +1,23 @@
 #pragma once
 
 #include <filesystem>
+#include <fstream>
 
 #include "utils/save-load/SaveData.hpp"
 
 class SaveFileHandler {
 private:
     SaveFileHandler() = default;
+
+    static void savePlayer(SaveData &saveData, std::ofstream &out);
+    static void saveProperty(SaveData &saveData, std::ofstream &out);
+    static void saveDeck(SaveData &saveData, std::ofstream &out);
+    static void saveLog(SaveData &saveData, std::ofstream &out);
+
+    static void loadPlayer(SaveData &saveData, std::ifstream &in, std::filesystem::path &path, int &line);
+    static void loadProperty(SaveData &saveData, std::ifstream &in, std::filesystem::path &path, int &line);
+    static void loadDeck(SaveData &saveData, std::ifstream &in, std::filesystem::path &path, int &line);
+    static void loadLog(SaveData &saveData, std::ifstream &in, std::filesystem::path &path, int &line);
 
 public:
     static void saveGame(SaveData saveData, std::filesystem::path path);
