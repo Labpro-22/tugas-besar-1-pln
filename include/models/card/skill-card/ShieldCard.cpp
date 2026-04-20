a03180a20e190562a78847b7488af612e7e45965
YeyThePotatoMan <klocesaragih@gmail.com>
2026-04-20 16:10:52 +0700

    feat: player effect

--- include/models/card/skill-card/ShieldCard.cpp
+++ include/models/card/skill-card/ShieldCard.cpp
@@ -3,6 +3,6 @@
 ShieldCard::ShieldCard(const std::string& message) : SkillCard(message) {}
 
 void ShieldCard::takeEffect(Player& p, GameManager& gm) {
-    p.setShielded(true, 1);
-    std::cout << "Tameng Aktif! Kebal dari bayar sewa/pajak untuk 1 putaran.\n";
+    p.addEffect(PlayerEffect("SHIELD", false, 1, 0));
+    std::cout << "Shield anda aktif! anda kebal dari bayar sewa/pajak untuk 1 putaran.\n";
 }
