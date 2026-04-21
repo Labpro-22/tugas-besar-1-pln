f06b07ab0c522a39968c8ce26769525f1f276bb1
Muhammad Nafis Habibi <137036100+NafisKreatif@users.noreply.github.com>
2026-04-21 20:07:28 +0700

    feat: implementation of getter in Log

--- src/utils/log/TransactionLog.cpp
+++ src/utils/log/TransactionLog.cpp
@@ -3,7 +3,12 @@
 TransactionLog::TransactionLog(int turn, std::string username, std::string action, std::string details)
     : turn{turn}, username{std::move(username)}, action{std::move(action)}, details{std::move(details)} {}
 
-std::string TransactionLog::toString()
+int TransactionLog::getTurn() const{ return turn; }
+std::string TransactionLog::getUsername() const{ return username; }
+std::string TransactionLog::getAction() const{ return action; }
+std::string TransactionLog::getDetails()const { return details; }
+
+std::string TransactionLog::toString() const
 {
     return std::to_string(turn) + " " + username + " " + action + " " + details;
 }
