a5ca9641b5f775f5f957bc126187d38e837b628a
YeyThePotatoMan <klocesaragih@gmail.com>
2026-04-21 14:12:09 +0700

    ref: skill card usage view

--- include/views/UseSkillCardView.hpp
+++ include/views/UseSkillCardView.hpp
@@ -9,4 +9,8 @@ class UseSkillCardView {
 public:
     int promptChooseCardToUse(const std::vector<SkillCard*>& cards);
     void processAndPlayCard(SkillCard* card, Player& player, GameManager& gm);
+
+    int askForTargetPosition();
+    Player* askForTargetPlayer(GameManager& gm);
+    int askForDemolitionTileId();   
 };
