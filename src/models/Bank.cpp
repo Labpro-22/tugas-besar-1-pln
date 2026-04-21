1355c586aeb83f918e2079bb3de98e45c9ca5963
Muhammad Nafis Habibi <137036100+NafisKreatif@users.noreply.github.com>
2026-04-21 19:22:17 +0700

    feat: Bank constructor

--- src/models/Bank.cpp
+++ src/models/Bank.cpp
@@ -1,7 +1,127 @@
 #include "models/bank/Bank.hpp"
+#include <algorithm>
+#include <vector>
 
-Bank::Bank(long long initialMoney) : initialMoney{initialMoney} {}
+Bank::Bank(long long initialMoney, Config &config) : initialMoney{initialMoney}
+{
+    for (PropertyConfig &property : config.properties) {
+        if (property.type == "STREET") {
+            properties.push_back(
+                StreetProperty{
+                    property.code,
+                    property.name,
+                    property.color,
+                    property.price,
+                    property.mortgageValue,
+                    1,
+                    0,
+                    property.housePrice,
+                    property.hotelPrice,
+                    property.rent});
+        }
+        else if (property.type == "RAILROAD") {
+            properties.push_back(
+                RailroadProperty{
+                    property.code,
+                    property.name,
+                    property.color,
+                    property.price,
+                    property.mortgageValue,
+                    1,
+                    0,
+                    config.railroadRent});
+        }
+        else if (property.type == "STREET") {
+            properties.push_back(
+                UtilityProperty{
+                    property.code,
+                    property.name,
+                    property.color,
+                    property.price,
+                    property.mortgageValue,
+                    1,
+                    0,
+                    config.utilityRent});
+        }
+    }
+}
 
+Bank::Bank(long long initialMoney, Config &config, std::vector<PropertySaveData> &saveData, std::vector<Player> &players) : initialMoney{initialMoney}
+{
+    for (PropertyConfig &propertyConfig : config.properties) {
+        for (PropertySaveData &propertyData : saveData) {
+            if (propertyConfig.code != propertyData.code) continue;
+            if (propertyConfig.type == "STREET") {
+                StreetProperty property{
+                    propertyConfig.code,
+                    propertyConfig.name,
+                    propertyConfig.color,
+                    propertyConfig.price,
+                    propertyConfig.mortgageValue,
+                    propertyData.festivalMultiplier,
+                    propertyData.festivalDuration,
+                    propertyConfig.housePrice,
+                    propertyConfig.hotelPrice,
+                    propertyConfig.rent};
+                if (propertyData.hasHotel) {
+                    property.buildHouse(4);
+                    property.buildHotel();
+                }
+                else {
+                    property.buildHouse(propertyData.houseCount);
+                }
+                auto player = std::find(players.begin(), players.end(), [propertyData](Player &p) {
+                    return p.getUsername() == propertyData.owner;
+                });
+                if (player != players.end()) {
+                    property.setOwner(player.base());
+                }
+                properties.push_back(property);
+            }
+            else if (propertyConfig.type == "RAILROAD") {
+                RailroadProperty property{
+                    propertyConfig.code,
+                    propertyConfig.name,
+                    propertyConfig.color,
+                    propertyConfig.price,
+                    propertyConfig.mortgageValue,
+                    propertyData.festivalMultiplier,
+                    propertyData.festivalDuration,
+                    config.railroadRent};
+                auto player = std::find(players.begin(), players.end(), [propertyData](Player &p) {
+                    return p.getUsername() == propertyData.owner;
+                });
+                if (player != players.end()) {
+                    property.setOwner(player.base());
+                }
+                properties.push_back(property);
+            }
+            else if (propertyConfig.type == "Utility") {
+                UtilityProperty property{
+                    propertyConfig.code,
+                    propertyConfig.name,
+                    propertyConfig.color,
+                    propertyConfig.price,
+                    propertyConfig.mortgageValue,
+                    propertyData.festivalMultiplier,
+                    propertyData.festivalDuration,
+                    config.utilityRent};
+                auto player = std::find(players.begin(), players.end(), [propertyData](Player &p) {
+                    return p.getUsername() == propertyData.owner;
+                });
+                if (player != players.end()) {
+                    property.setOwner(player.base());
+                }
+                properties.push_back(property);
+            }
+        }
+    }
+}
+
+std::vector<Property> &Bank::getProperties()
+{
+    return properties;
+}
 
 void Bank::giveInitialMoney(std::vector<Player> &players)
 {
