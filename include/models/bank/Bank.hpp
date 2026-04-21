1240d60dda12b17aa8d9d23972670504929f1bac
Muhammad Nafis Habibi <137036100+NafisKreatif@users.noreply.github.com>
2026-04-21 19:22:17 +0700

    feat: Bank constructor

--- include/models/bank/Bank.hpp
+++ include/models/bank/Bank.hpp
@@ -3,14 +3,22 @@
 #include <vector>
 
 #include "models/player/Player.hpp"
+#include "models/property/Property.hpp"
+#include "models/property/StreetProperty.hpp"
+#include "models/property/RailroadProperty.hpp"
+#include "models/property/UtilityProperty.hpp"
 
 class Bank {
 private:
     long long initialMoney;
+    std::vector<Property> properties;
 
 public:
-    Bank(long long initialMoney);
-    
+    Bank(long long initialMoney, Config &config);
+    Bank(long long initialMoney, Config &config, std::vector<PropertySaveData> &saveData, std::vector<Player>& players);
+
+    std::vector<Property>& getProperties();
+
     void giveInitialMoney(std::vector<Player> &players);
     void giveMoney(Player &player, long long amount);
     void giveMoneyToAll(std::vector<Player> &players, long long amount);
