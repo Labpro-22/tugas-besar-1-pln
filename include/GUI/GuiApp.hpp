3923f2de109aba609a8cb0c865ef87bc7776994d
YeyThePotatoMan <klocesaragih@gmail.com>
2026-04-23 23:22:22 +0700

    fix: GUI app

--- include/GUI/GuiApp.hpp
+++ include/GUI/GuiApp.hpp
@@ -1,6 +1,9 @@
 #include <SFML/Graphics.hpp>
 #include <memory>
 #include "IGuiState.hpp"
+#include "animation/PieceAnimator.hpp"
+#include "renderer/BoardRenderer.hpp"
+#include "renderer/Console.hpp"
 
 class GuiApp {
 public:
@@ -16,5 +19,8 @@ private:
     sf::Font m_font;
     sf::Font m_bold;
     std::unique_ptr<IGuiState>  m_state;
+    PieceAnimator m_anim;
+    std::unique_ptr<BoardRenderer> m_board;
+    std::unique_ptr<ConsolePanel>  m_console;
     sf::Clock m_clock;
 };
