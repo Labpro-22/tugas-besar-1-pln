72c692af336a73cad209c9b090413f7ddecd06db
YeyThePotatoMan <klocesaragih@gmail.com>
2026-04-23 23:23:24 +0700

    fix: main run with argv

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,14 @@
 #include "core/Nimonspoli.hpp"
+#include "GuiState.hpp"
+#include "GuiApp.hpp"
 
-int main() {
-    Nimonspoli::run();
-
+int main(int argc, char *argv[]) {
+    if( argc == 1 && argv[0] == "gui"){
+        auto state = std::make_unique<GuiState>();
+        GuiApp app(std::move(state));
+    }
+    else {
+        Nimonspoli::run();
+    }
     return 0;
 }
