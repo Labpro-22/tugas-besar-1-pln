e3bc898cbcb56ffd548df9885e79f9d809132886
YeyThePotatoMan <klocesaragih@gmail.com>
2026-04-23 19:51:07 +0700

    feat: dice rolling

---
+++ include/GUI/renderer/Dice.hpp
@@ -0,0 +1,15 @@
+#pragma once
+#include <SFML/Graphics.hpp>
+#include "IGuiState.hpp"
+
+class DiceWidget {
+public:
+    DiceWidget(const sf::Font& font);
+    void update(float dt);
+    void draw(sf::RenderWindow& win, float x, float y, const DiceInfo& dice);
+
+private:
+    void drawDie(sf::RenderWindow& win, float x, float y, int face, bool spinning, float spinAngle);
+    const sf::Font& m_font;
+    float m_spinAngle = 0.f;
+};
