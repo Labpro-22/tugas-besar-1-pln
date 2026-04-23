cc60c8a099cc2795d07183984c5661618bf1d60c
An-Dafa <andafaanzaa@gmail.com>
2026-04-23 19:00:55 +0700

    fix: fixing Tile logic
    After all of that, we can implemented new Tile logic that suitable with the design. Also not fully implemented yet. After this I will work for ChanceCardTile and FestiveTile

--- src/models/tile/special-tile/tax-tile/IncomeTaxTile.cpp
+++ src/models/tile/special-tile/tax-tile/IncomeTaxTile.cpp
@@ -3,29 +3,5 @@
 #include "core/GameManager.hpp"
 
 void IncomeTaxTile::onLanded(Player& p, GameManager& gm) {
-    // Jujur bingung implementasinya gimana, gtw caranya kalau pemain milih bayar flat atau percentage, jadi mungkin uncomment aja yang dibutuhkan
-    
-    // Jika flat
-    // Config config = gm.getConfig();
-    // long long incomeFlatTax = config.getIncomeFlatTax();
-    // if (p.getMoney() < incomeFlatTax) {
-    //     p.bankruptByBank();
-    //     return;
-    // }
-    // p.payTax(incomeFlatTax);
-   
-    // ===========================================================
-
-    // Jika percentage
-    // Config config = gm.getConfig();
-    // long long incomePercentageTax = config.getIncomePercentageTax();
-    // long long assetsWealth = 0;
-    // vector<Property*> properties = p.getProperties();
-    // for (int i = 0; i < properties.size(); i++) {
-    //     assetsWealth += properties[i]->calculateAssetValue();
-    // }
-    // long long totalWealth = p.getMoney() + assetsWealth;
-    // p.payTax(totalWealth * incomePercentageTax / 100);
-
-
+    gm.processPayIncomeTax();
 }  
